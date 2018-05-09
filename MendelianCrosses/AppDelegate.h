28704747ddce6a0cc339a0e33e772d5df576c090
Jing Wei Li <ljw9801055@gmail.com>
2018-05-09 15:29:54 -0400

    Initial Commit

---
+++ MendelianCrosses/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MendelianCrosses
+//
+//  Created by Jing Wei Li on 5/9/18.
+//  Copyright © 2018 Jing Wei Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MendelianCrosses/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MendelianCrosses
+//
+//  Created by Jing Wei Li on 5/9/18.
+//  Copyright © 2018 Jing Wei Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
